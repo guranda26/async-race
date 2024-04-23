052e52be3b52eb83933bd5197b55f8017d31f861
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-04-23 22:09:57 +0400

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, ''),
-  },
 });
-
